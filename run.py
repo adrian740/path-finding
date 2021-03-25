1f23f491692fa11d999184bde8cdd3173f9c849c
Adrian <burnobostock@gmail.com>
2021-03-25 15:13:03 +0100

    s

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 data1 = np.genfromtxt("ERDA 3c - Sheet1.csv", delimiter=",")
 data2 = np.genfromtxt("cities south holland.csv", delimiter=",")
 
-plt.scatter(data1[:,1], data1[:,2])
-plt.scatter(data2[:,1], data2[:,2])
+plt.scatter(data1[:,1], data1[:,2], label="Parks")
+plt.scatter(data2[:,1], data2[:,2], label="Cities")
+
+plt.scatter([52.0186, 52.1763], [4.3782, 4.5282], label="Delft, Leiden")
+
+plt.legend()
 plt.show()
